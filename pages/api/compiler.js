3191e9d646180d0162509f9f2960bfde3485369b
AlanPan0131 <panalan0131@gmail.com>
2022-12-28 22:10:14 +0800

    Update compiler.js

--- pages/api/compiler.js
+++ pages/api/compiler.js
@@ -1,14 +1,16 @@
-import * as child from 'child_process';
+// import * as child from 'child_process';
+import os from 'os';
 
 export default function handler(req, res) {
     // const { cmd } = req.query;
-    var p = child.exec("g++", (error, stdout, stderr) => {
-        if (error) {
-            res.status(400).end(error.toString())
-        } else if (stderr) {
-            res.status(400).end(stderr.toString())
-        } else {
-            res.end("stdout:\n"+stdout)
-        }
-    });
+    // var p = child.exec("g++", (error, stdout, stderr) => {
+    //     if (error) {
+    //         res.status(400).end(error.toString())
+    //     } else if (stderr) {
+    //         res.status(400).end(stderr.toString())
+    //     } else {
+    //         res.end("stdout:\n"+stdout)
+    //     }
+    // });
+    res.end("type:" + os.type() + "\nrelease:" + os.release() + "\nplatform:" + os.platform())
 }
